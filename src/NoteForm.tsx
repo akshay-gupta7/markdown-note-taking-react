02bb7eee3fbb0bb1ed39b59cd75450e2001d2231
Akshay Gupta <gravity.akshay@gmail.com>
2023-03-07 01:01:51 +0530

    Add Title and Tags columns in NoteForm

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -4,7 +4,20 @@ export function NoteForm(){
     return (
     <Form>
         <Stack gap={4}>
-
+            <Row>
+                <Col>
+                    <Form.Group controlId="title">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control required />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group controlId="tags">
+                        <Form.Label>Tags</Form.Label>
+                        <Form.Control required />
+                    </Form.Group>
+                </Col>
+            </Row>
         </Stack>
     </Form>
     )
