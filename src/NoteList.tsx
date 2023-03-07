ee0f1421c8a4d630d119c9da3a8352465b373035
Akshay Gupta <gravity.akshay@gmail.com>
2023-03-08 02:04:24 +0530

    Add NoteCard styling and data

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,4 +1,4 @@
-import { Row, Col, Stack, Button, Form, Card } from "react-bootstrap"
+import { Row, Col, Stack, Button, Form, Card, Badge} from "react-bootstrap"
 import { Link } from "react-router-dom"
 import ReactSelect from "react-select"
 import { useState, useMemo } from "react"
@@ -84,7 +84,18 @@ export function NoteList({ availableTags, notes }: NoteListProps){
 function NoteCard({ id, title, tags}: SimplifiedNote){
     return <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
         <Card.Body>
-
+            <Stack gap={2} className = "align-items-center justify-content-center h-100">
+                <span className="fs-5">{title}</span>
+                {tags.length > 0 && (
+                    <Stack gap={1} direction="horizontal" className="justify-content-center flex-wrap">
+                        {tags.map(tag => (
+                            <Badge className="text-truncate" key={tag.id}>
+                                {tag.label}
+                            </Badge>
+                        ))}
+                    </Stack>
+                )}
+            </Stack>
         </Card.Body>
     </Card>
 }
